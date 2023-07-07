c18b19d3befbbd4e13875b25740753c9f9f4f4d2
Weil <f0955710119@gmail.com>
2023-07-07 16:22:28 +0800

    [fix] ensure tickets can be all loop

--- contracts/libs/Lottery.sol
+++ contracts/libs/Lottery.sol
@@ -5,21 +5,28 @@ library Lottery {
     function _checkLotteryPrize(
         uint256[] memory drawNumber,
         uint256[4] memory lotteryNumber
-    ) public pure returns (uint256 prizeFactor) {
-        for (uint256 i = 0; i < lotteryNumber.length; i++) {
+    ) public pure returns (uint256) {
+        uint256 prizeFactor;
+
+        for (uint256 i = 0; i < lotteryNumber.length; ) {
             if (drawNumber[i] == lotteryNumber[i]) {
                 prizeFactor += 1;
             }
+            // prettier-ignore
+            unchecked { ++i; }
         }
+
+        return prizeFactor;
     }
 
     function _calcPrizeWithFactor(
         uint256 _reward,
         uint256 _factor
-    ) public pure returns (uint256 reward) {
-        if (_factor == 1) reward = _reward;
-        if (_factor == 2) reward = (_reward * 12) / 10;
-        if (_factor == 3) reward = _reward * 2;
-        if (_factor == 4) reward = _reward * 5;
+    ) public pure returns (uint256) {
+        if (_factor == 1) return _reward;
+        if (_factor == 2) return (_reward * 12) / 10;
+        if (_factor == 3) return _reward * 5;
+        if (_factor == 4) return _reward * 10;
+        return (_reward * 8) / 10;
     }
 }
